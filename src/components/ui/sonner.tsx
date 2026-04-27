c2616de5e007f48209bf329ec8b6ae96d99c89f0
veil <shenosghosting@gmail.com>
2026-04-27 19:35:36 +0200

    Refactor Toaster component for cleaner syntax

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,9 +1,5 @@
 import { Toaster as SonnerToaster } from "sonner";
 
 export const Toaster = ({ ...props }) => (
-  <SonnerToaster
-    theme="dark"
-    className="toaster group"
-    {...props}
-  />
+  <SonnerToaster theme="dark" className="toaster group" {...props} />
 );
